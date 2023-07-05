0004fe01fedebb8f5868d2142d4227dd7e3764ba
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-07-05 14:02:48 +0100

    Styling sub-hero-section

--- expenses-calculator/src/components/Container.jsx
+++ expenses-calculator/src/components/Container.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import "./styles/Container.scss"
+import logo from "../images/EXcal-logo-03.png"
+import subImage from "../images/sub-hero-image.png"
 
 function Container() {
   return (
     <div className='component-container'>
       <div className="hero-section">
-        <h3>Hi</h3>
-        <h1>Be in <span className='bold'>charge</span> of your <span className='bold'>Expenses!</span></h1>
+
+        <nav>
+          <img className='logo' src={logo} alt='logo'/>
+        </nav>
+
+        <main>
+          <h1>Be in charge <br/> of your Expenses!</h1>
+          <p>With EXcal, you will always be one step ahead of all your financial expenses. No late surprises!</p>
+          
+          <div className='button-wrapper'>
+          <button>Get Started</button>
+          </div>
+        </main>
+
+      </div>
+
+      <div className='sub-hero-section'>
+        <img className='sub-image' src={subImage} alt='sub-img' />
       </div>
     </div>
   )
