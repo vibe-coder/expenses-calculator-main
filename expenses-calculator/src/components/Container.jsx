d7bf71de98d6706e950a270cbec7b91749b2c971
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-07-05 11:05:59 +0100

    Styling Hero Section

---
+++ expenses-calculator/src/components/Container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./styles/Container.scss"
+
+function Container() {
+  return (
+    <div className='component-container'>
+      <div className="hero-section">
+        <h3>Hi</h3>
+        <h1>Be in <span className='bold'>charge</span> of your <span className='bold'>Expenses!</span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default Container
