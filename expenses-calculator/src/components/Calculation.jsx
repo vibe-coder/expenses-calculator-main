1ae41d7c0addc38d3b3e2edaec8fc77887f89df0
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-07-05 14:07:18 +0100

    Setting up calculations section

---
+++ expenses-calculator/src/components/Calculation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./styles/Calculation.scss"
+
+function Calculation() {
+  return (
+    <div className='calculation-container'>
+      <div className="income-container">
+        Income
+      </div>
+    </div>
+  )
+}
+
+export default Calculation
